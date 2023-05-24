7c65b38ffbdc1d04c804976d0eed2f66910b2314
Daniel Castillo <danielcastillo89@icloud.com>
2023-05-24 13:29:09 -0500

    getting todos from server

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+  }
+  fetchAllTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({...this.state, todos: res.data.data})
+      })
+      .catch()
+  }
+  componentDidMount() {
+    this.fetchAllTodos()
+  }
   render() {
-    return null
+    return (
+      this.state.todos.map(td => {
+        return <div key={td.id}>{td.name}</div>
+      })
+    )
   }
 }
